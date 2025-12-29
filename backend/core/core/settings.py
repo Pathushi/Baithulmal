5f6527763ff698fdc4731e3216129c06b98c0585
Pathushi <pathushi.m@gmail.com>
2025-12-29 20:21:27 +0530

    Updated

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -125,7 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email configuration
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
